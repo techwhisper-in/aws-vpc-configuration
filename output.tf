f997e96da2d811163880136820c02457c10846e8
techwhisper-in <technicalcodelab@gmail.com>
2024-07-04 13:11:43 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,24 @@
-output "route" {
+output "vpc_id" {
+  value=aws_vpc.my_vpc.id
+}
+output "internet_gateway" {
+  value = aws_internet_gateway.my_igw.id
+}
+output "public_route_table" {
+  value = aws_route_table.public_route_table.id
+}
+output "public_subnets" {
+  value = [for subnet in aws_subnet.public_subnet : subnet.id]
+}
+output "public_route_table_association" {
+  value = [for association in aws_route_table_association.public_subnet_association : association.id]
+}
+output "private_route_table" {
   value = aws_route_table.private_route_table.id
 }
-
+output "private_subnets" {
+  value = [for subnet in aws_subnet.private_subnet : subnet.id]
+}
+output "private_route_table_association" {
+  value = [for association in aws_route_table_association.private_subnet_association : association.id]
+}
