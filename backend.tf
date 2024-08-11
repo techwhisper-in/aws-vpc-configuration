3f1613c83fcaf4cb03dfe662985fffee01043c16
Ankit Kumar <technicalcodelab@gmail.com>
2024-08-11 11:43:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "techwhisper-ami-snapshot-ebs-volume-inventory"
-    key            = "file/deploy-vpc.tfstate"
+    key            = "file/create-vpc.tfstate"
     region         = "ap-south-1"
   }
 }
